686481799ef6147501668617439254d4a1b16df5
Anthony <anthonyjlower@gmail.com>
2018-01-16 17:29:09 -0600

    Created new form/template  and route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const User = require("../models/user.js")
+const User = require("../models/user.js");
 
 
 
+
+
+
+router.route("/new")
+	.get((req, res) => {
+		res.render("users/new.ejs")
+	})
 
 
 
